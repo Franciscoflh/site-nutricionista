7a7e552a5cba1dc17d9ce346fba5106911e6c771
Lauro Caetano <116907312+lauroabreu@users.noreply.github.com>
2023-05-10 12:39:13 -0300

    topo adicionado

--- javascript/scriptTopo.js
+++ javascript/scriptTopo.js
@@ -1,20 +1,3 @@
-let count = 1;
-
-document.getElementById("radio1").checked = true;
-
-setInterval(function(){
-    nextImage();
-}, 4000)
-
-function nextImage(){
-    count++;
-    if(count > 4){
-        count=1;
-    }
-
-    document.getElementById("radio"+count).checked = true;
-}
-
 
 const btn = document.getElementById("btn-top");
 
